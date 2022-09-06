dceac47918be6e42a38d85c94bd4481e0c3755ef
Harshit <greywolff711@gmail.com>
2022-09-06 19:54:12 +0530

    added the user registration module

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,12 @@
 const express=require('express');
 const router=express.Router();
+const gravatar=require('gravatar');
+const jwt=require('jsonwebtoken');
+const config=require('config');
 const { check, validationResult } = require('express-validator');
+const bcrypt=require('bcryptjs');
+
+const User=require('../../models/User');
 
 //@route  POST/users
 //@desc   Register user
@@ -15,13 +21,52 @@ router.post(
         check('email','Email is required').isEmail(),
         check('password','Please enter a password with 6 or more letters').isLength({ min: 5 }),
     ],
-    (req,res)=>{
+    async (req,res)=>{
         errors=validationResult(req);
         if(!errors.isEmpty()){
             return res.status(400).json({errors:errors.array()});
         }
-        console.log(req.body);
-        res.send('User route');
+        
+        const {name,email,password}=req.body;
+
+        try{
+            //See if user is present
+            let user=await User.findOne({email});
+            if(user){
+                return res.status(400).json({error:[{msg:'User already taken'}]});
+            }
+            //use gravatar
+            const avatar=gravatar.url(email,{
+                s:'200',//size
+                r:'pg',//rating
+                d:'mm'//default profile image
+            })
+
+            user=new User({
+                name,
+                email,
+                avatar,
+                password
+            })
+            //encrypt password as it is plane text
+            const salt=await bcrypt.genSalt(10);
+            user.password=await bcrypt.hash(password,salt);
+            await user.save();
+            //return json web token
+            const payload={
+                user:{
+                    id:user.id
+                }
+            }
+            jwt.sign(payload,config.get('jwttoken'),{expiresIn:3600},(err,token)=>{
+                if(err)throw err;
+                res.json({token});
+            });
+        }
+        catch(err){
+            console.log(err.message);
+            res.status(500).send('Server Error');
+        }
     }
 );
 
