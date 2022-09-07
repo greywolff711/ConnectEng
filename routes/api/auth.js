ace1a55bde379f219c0e33036533bbd4fe6b3990
Harshit <greywolff711@gmail.com>
2022-09-07 11:11:55 +0530

    added the profile routes

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,10 +1,69 @@
 const express=require('express');
 const router=express.Router();
+const auth=require('../../middleware/auth');
+const User=require('../../models/User');
+const jwt=require('jsonwebtoken');
+const config=require('config');
+const { check, validationResult } = require('express-validator');
+const bcrypt=require('bcryptjs');
 
 //@route  GET/auth
 //@desc   Test route
 //@access Public
 
-router.get('/',(req,res)=>res.send('AUTH PAGE'));
+router.get('/',auth,async (req,res)=>{
+    try{
+        const user=await User.findById(req.user.id).select('-password');
+        //req.user can be accessed throughout the middleware because of its definitation in auth.js in middleware
+        res.json(user);
+    }
+    catch(err){
+        console.log(err.message);
+    }
+});
+
+//@route  POST/auth
+//@desc   posting auth route
+//@access Public
+router.post(
+    '/',
+    [
+        check('email','Email is required').isEmail(),
+        check('password','Password error').exists(),
+    ],
+    async (req,res)=>{
+        errors=validationResult(req);
+        if(!errors.isEmpty()){
+            return res.status(400).json({errors:errors.array()});
+        }
+        
+        const {email,password}=req.body;
+
+        try{
+            //See if user is present
+            let user=await User.findOne({email});
+            if(!user){
+                return res.status(400).json({error:[{msg:'Invalid credentials'}]});
+            }
+
+            const isMatch=await bcrypt.compare(password,user.password);
+            if(!isMatch)return res.status(400).json({error:[{msg:'Invalid credentials'}]});
+            //return json web token
+            const payload={
+                user:{
+                    id:user.id
+                }
+            }
+            jwt.sign(payload,config.get('jwttoken'),{expiresIn:3600},(err,token)=>{
+                if(err)throw err;
+                res.json({token});
+            });
+        }
+        catch(err){
+            console.log(err.message);
+            res.status(500).send('Server Error');
+        }
+    }
+);
 
 module.exports=router;
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,10 +1,92 @@
 const express=require('express');
 const router=express.Router();
+const auth=require('../../middleware/auth');
+const Profile=require('../../models/Profile');
+const { check, validationResult } = require('express-validator');
+const { json } = require('express');
 
-//@route  GET/profile
-//@desc   Test route
-//@access Public
+//@route  GET api/profile/me
+//@desc   get the user details
+//@access Private
 
-router.get('/',(req,res)=>res.send('profile PAGE'));
+router.get('/me',auth,async(req,res)=>{
+    try{
+        const profile=await Profile.findOne({user:req.user.id}).populate('user',['name','avatar']);
+        if(!profile)return res.status(400).json({msg:'No Profile found'});
+        res.json(profile);
+    }
+    catch(err){
+
+    }
+});
+
+//@route  POST api/profile
+//@desc   post the user details
+//@access Private
+
+router.post('/',
+    auth,
+    check('status', 'Status is required').notEmpty(),
+    check('skills', 'Skills is required').notEmpty(),
+    async (req,res)=>{
+        const errors=validationResult(req);
+        if(!errors.isEmpty())return res.status(400).json({errors:errors.array()});
+        //Start building profile object
+        const {
+            website,
+            skills,
+            company,
+            location,
+            bio,
+            status,
+            youtube,
+            twitter,
+            linkedin,
+            instagram,
+        } = req.body;
+        const profileFields={};
+        profileFields.user=req.user.id;
+        if(company)profileFields.company=company;
+        if(website)profileFields.company=company;
+        if(location)profileFields.location=location;
+        if(bio)profileFields.bio=bio;
+        if(status)profileFields.status=status;
+        if(skills)profileFields.skills=skills.split(',').map(skill=>skill.trim());
+
+        //Build Social object
+        profileFields.social={};
+
+        if(youtube)profileFields.social.youtube=youtube;
+        if(twitter)profileFields.social.twitter=twitter;
+        if(linkedin)profileFields.social.linkedin=linkedin;
+        if(instagram)profileFields.social.instagram=instagram;
+        console.log(profileFields.skills);
+
+        try{
+            let profile=await Profile.findOne({user:req.user.id});
+
+            //If profile is found
+            if(profile){
+                profile=await Profile.findOneAndUpdate(
+                    {user:req.user.id},
+                    {$set:profileFields},
+                    {new: true}
+                )
+
+                return res.json(profile);
+            }
+
+            //New profile
+            profile=new Profile(profileFields);
+            await profile.save();
+            return res.json(profile);
+        }
+        catch(err){
+            console.log(err.message);
+            res.status(500).json({msg:'Server Error'})
+        }
+
+        res.send('Hello');
+    })
 
 module.exports=router;
