43ceaf66b83c7d42b667daa04a58db118d739414
Harshit <greywolff711@gmail.com>
2022-09-08 13:54:53 +0530

    Added comments routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -84,4 +84,79 @@ router.post('/',
             res.status(500).send('Server Error');
         }
     })
+
+    //@route  PUT api/post/like/:id
+    //@desc   like post
+    //@access Private
+    router.put('/like/:id',auth,async(req,res)=>{
+        try {
+            const post=await Post.findById(req.params.id);
+            
+            if (post.likes.some((like) => like.user.toString() === req.user.id)) {
+                return res.status(400).send('Msg already liked');
+            }
+
+            post.likes.unshift({user:req.user.id});
+            await post.save();
+            res.json(post.likes);
+        } 
+        catch (err) {
+            console.log(err.message);
+            res.status(500).send('Server Error');
+        }
+    })
+
+    //@route  PUT api/post/unlike/:id
+    //@desc   dislike post
+    //@access Private
+    router.put('/unlike/:id',auth,async(req,res)=>{
+        try {
+            const post=await Post.findById(req.params.id);
+            
+            if (!post.likes.some((like) => like.user.toString() === req.user.id)) {
+                return res.status(400).send('Msg has not been liked yet');
+            }
+
+            const removedIndex=post.likes.map(like=>like.user.toString()).indexOf(req.user.id);
+            post.likes.splice(removedIndex,1);
+            await post.save();
+            res.json(post.likes);
+        } 
+        catch (err) {
+            console.log(err.message);
+            res.status(500).send('Server Error');
+        }
+    })
+
+    //@route  POST api/post/comment/:id
+    //@desc   comment on a post
+    //@access Private
+    router.post('/comment/:id',
+    auth,
+    check('text','Text is required').not().isEmpty(),
+    async(req,res)=>{
+        errors=validationResult(req);
+        if(!errors.isEmpty()){
+            return res.status(400).json({errors:errors.array()});
+        }
+
+        const user=User.findById(req.user.id).select('-password');
+        try {
+            const post=await Post.findById(req.params.id);
+            const newComment={
+                user:req.user.id,
+                text:req.body.text,
+                name:user.name,
+                avatar:user.avatar
+            }
+            post.comments.unshift(newComment);
+
+            await post.save();
+            res.json(post.comments);
+        } 
+        catch (err) {
+            console.log(err.message);
+            res.status(500).send('Server Error');
+        }
+    })
 module.exports=router;
