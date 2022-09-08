d07aff4d4ffaef1d46e84b39878fb0f0a661e273
Harshit <greywolff711@gmail.com>
2022-09-08 12:32:45 +0530

    created a post route to add a new post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,10 +1,39 @@
 const express=require('express');
 const router=express.Router();
+const { check, validationResult } = require('express-validator');
+const auth=require('../../middleware/auth');
+const User=require('../../models/User');
+const Post=require('../../models/Post');
+const Profile=require('../../models/Profile');
 
-//@route  GET/post
-//@desc   Test route
-//@access Public
+//@route  POST api/post
+//@desc   post a request
+//@access Private
 
-router.get('/',(req,res)=>res.send('post PAGE'));
+router.post('/',
+    auth,
+    check('text','text is required').not().isEmpty(),
+    async (req,res)=>{
+        errors=validationResult(req);
+        if(!errors.isEmpty()){
+            return res.status(400).json({errors:errors.array()});
+        }
+        try {
+            const user=User.findById(req.user.id).select('-password');
+            const newPost = new Post({
+                text: req.body.text,
+                name: user.name,
+                avatar: user.avatar,
+                user: req.user.id
+              });
+            
+            const post=await newPost.save();
+            res.json(post);
+        } catch (err) {
+            console.log(err);
+            res.status(500).send('Server Error');
+        }
+
+    });
 
 module.exports=router;
